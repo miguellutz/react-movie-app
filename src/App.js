f2ca9f602682df723cb939e9cdb312d1f7c01262
Miguel Lutz <miguellutz31@gmail.com>
2023-01-09 19:10:58 +0100

    Fetch movies from omdb API and display dynamically through Movie component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from "react";
 
-import './App.css'
-import SearchIcon from './search.svg'
+import "./App.css";
+import SearchIcon from "./search.svg";
+import Movie from "./Movie";
 
-const API_URL = 'http://www.omdbapi.com?apikey=748b369c'
+const API_URL = "http://www.omdbapi.com?apikey=748b369c";
 
 const App = () => {
+  const [movieTitle, setMovieTitle] = useState("");
+
+  const [movies, setMovies] = useState([]);
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
-    console.log(data.Search)
-  }
+    setMovies(data.Search);
+  };
 
   useEffect(() => {
-    searchMovies('superman')
-  }, [])
+    searchMovies("superman");
+  }, []);
 
-  return(
-    <h1>Hello World</h1>
+  return (
+    <div className='app'>
+      <h1>Netflix</h1>
+
+      <div className='search'>
+        <input
+          type='text'
+          name='Movie Title'
+          id='Movie Title'
+          placeholder='Search for Movies'
+          value={movieTitle}
+          onChange={(e) => {
+            setMovieTitle(e.target.value);
+          }}
+        />
+        <img
+          src={SearchIcon}
+          alt='search'
+          onClick={() => {
+            searchMovies(movieTitle);
+          }}
+        />
+      </div>
+
+
+      {movies.length > 0 ? (
+        <div className='container'>
+          {movies.map((movie) => {
+            return <Movie movie={movie} />;
+          })}
+        </div>
+      ) : (
+        <div className='empty'>
+          <h2>No movies found</h2>
+        </div>
+      )}
+    </div>
   );
-}
+};
 
-export default App
+export default App;
