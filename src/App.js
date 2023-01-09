8c9b9d657489af73c8edca1c761fdd65e93646e8
Miguel Lutz <miguellutz31@gmail.com>
2023-01-09 17:21:42 +0100

    Added svg search icon and imported

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import { useEffect } from 'react'
+import { useEffect } from 'react';
+
+import './App.css'
+import SearchIcon from './search.svg'
 
 const API_URL = 'http://www.omdbapi.com?apikey=748b369c'
 
